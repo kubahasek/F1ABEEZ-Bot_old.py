749e1e11b1722ab86cc4045d8f504278608ba839
Jakub Hašek <hasek.kuba@hotmail.cz>
2021-11-08 20:04:58 +0100

    Changed academymsg

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ async def raceMSGf2(ctx):
 @commands.has_any_role("Admin", "Moderator", "Trialist Manager")
 async def academyMSG(ctx):
   await ctx.message.delete()
-  msg =  await ctx.send(f"<@&{info.academyRole}>\n**TRIAL RACE BRIEFING:**\nWelcome to the F1ABEEZ trial race! I would just like to run through what is expected of you from your trial:\n- Please drive clean - we are a clean racing league, show respect to your fellow drivers! dirty driving will not be tolerated\n- Drive fast! It's still a race after all, we would like to see a true reflection of your pace\n- Do not use medium tyres in Qualifying for this trial race, as this lets us compare your quali pace!\n- Have fun! That's what we're all here for\n\nThe format is short qualifying, 25% race\nAfter the race is completed, <@401204069890523137> will DM you individually with our decision\nPlease react with a thumbs up once you have read this, good luck!")
+  msg =  await ctx.send(f"<@&{info.academyRole}>\n**TRIAL RACE BRIEFING:**\nWelcome to the F1ABEEZ trial race! I would just like to run through what is expected of you from your trial:\n- Please drive clean - we are a clean racing league, show respect to your fellow drivers! dirty driving will not be tolerated\n- Drive fast! It's still a race after all, we would like to see a true reflection of your pace\n- Do not use medium tyres in Qualifying for this trial race, as this lets us compare your quali pace!\n- Have fun! That's what we're all here for\n\nThe format is short qualifying, 25% race\nAfter the race is completed, one of the trialist leaders will DM you individually with their decision\nPlease react with a thumbs up once you have read this, good luck!")
   await msg.add_reaction("👍")
 
 @bot.command(name="ban")
