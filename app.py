a6d94aae813af2ee698f0062f35257df29ed415c
Jakub Hašek <hasek.kuba@hotmail.cz>
2021-12-16 17:26:51 +0100

    added command for creating new channel names

--- app.py
+++ app.py
@@ -704,6 +704,16 @@ async def youtube(ctx):
     print("youtube")
     print(e)
 
+@bot.command("channelname")
+async def channelName(ctx, name):
+  nameDic = {"a": "ᴀ","b": "ʙ", "c": "ᴄ", "d":  "ᴅ", "e": "ᴇ", "f":"ꜰ", "g": "ɢ", "h":"ʜ", "i":"ɪ", "j":"ᴊ", "k":"ᴋ", "l":"ʟ", "m":"ᴍ", "n": "ɴ", "o": "ᴏ", "p":"ᴘ", "q":"Q", "r":"ʀ", "s":"ꜱ", "t":"ᴛ", "u":"ᴜ", "v":"ᴠ", "w":"ᴡ", "x":"x", "y":"ʏ", "z":"ᴢ", "-":"-"}
+  returnName = ""
+  for i in range(len(name)):
+    char = name[i].lower()
+    returnName += nameDic.get(char)
+
+  await ctx.send("︱" + returnName)
+
 ## currently unsused, but saved if needed in the future
 
 # @bot.event
