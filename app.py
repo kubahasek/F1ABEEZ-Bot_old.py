407e6e6766b5ee7ea246aade7944c1e67306042b
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-02-01 09:07:00 +0100

    added logic into on_member_remove

--- app.py
+++ app.py
@@ -714,7 +714,7 @@ async def on_member_join(member):
 @bot.event
 async def on_member_remove(member):
   memberName = member.name
-  channel = bot.get_channel(info.leavingChannel)
+  channel = bot.get_channel(info.get_channelID(member.guild.id, "leavingChannel"))
   await channel.send(f"**{memberName}** has left the server.")
 
 @bot.command(name="stewardsdecisions")
