5fd2d6bc4310903743ad3d61366098c9c9e76200
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-02-03 23:07:30 +0100

    replaced followup with send

--- cogs/Announcements.py
+++ cogs/Announcements.py
@@ -1,3 +1,4 @@
+from code import InteractiveInterpreter
 from discord import SelectOption
 from nextcord import Interaction, SlashOption, ChannelType
 from nextcord.abc import GuildChannel
@@ -20,17 +21,16 @@ class Announcements(commands.Cog):
         roles = interaction.user.roles
         rolesToFind = ["Admin", "Moderator", "Trialist Manager"]
         await interaction.response.send_message(f"Working on it...")
-        followup = interaction.followup
         if(utils.check_roles(roles, rolesToFind)):
             await interaction.delete_original_message()
             academyID = info.get_roleID(interaction.guild.id, "academyRole")
-            await followup.send(f"<@&{academyID}>\n**TRIAL RACE BRIEFING:**\nWelcome to the F1ABEEZ trial race! I would just like to run through what is expected of you from your trial:\n- Please drive clean - we are a clean racing league, show respect to your fellow drivers! dirty driving will not be tolerated\n- Drive fast! It's still a race after all, we would like to see a true reflection of your pace\n- Do not use medium tyres in Qualifying for this trial race, as this lets us compare your quali pace!\n- Have fun! That's what we're all here for\n\nThe format is short qualifying, 25% race\nAfter the race is completed, one of the trialist leaders will DM you individually with their decision\nPlease react with a thumbs up once you have read this, good luck!")
+            await interaction.send(f"<@&{academyID}>\n**TRIAL RACE BRIEFING:**\nWelcome to the F1ABEEZ trial race! I would just like to run through what is expected of you from your trial:\n- Please drive clean - we are a clean racing league, show respect to your fellow drivers! dirty driving will not be tolerated\n- Drive fast! It's still a race after all, we would like to see a true reflection of your pace\n- Do not use medium tyres in Qualifying for this trial race, as this lets us compare your quali pace!\n- Have fun! That's what we're all here for\n\nThe format is short qualifying, 25% race\nAfter the race is completed, one of the trialist leaders will DM you individually with their decision\nPlease react with a thumbs up once you have read this, good luck!")
             last = interaction.channel.last_message_id
             msg = await interaction.channel.fetch_message(int(last))
             await msg.add_reaction("👍")
         else:
             await interaction.delete_original_message()
-            await followup.send("You do not have permission to use this command!")
+            await interaction.send("You do not have permission to use this command!")
 
 def setup(bot):
     bot.add_cog(Announcements(bot))
