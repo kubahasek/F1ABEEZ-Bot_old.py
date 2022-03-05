10cee09ca2cbaf3fa1d3e60d4c6183c199d80cac
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-03-05 20:06:27 +0100

    added clearchannel command

--- app.py
+++ app.py
@@ -580,6 +580,18 @@ async def channelName(interaction: Interaction, channelName: str = SlashOption(n
   else:
     await interaction.send("You do not have permission to use this command!")
 
+@bot.slash_command(name="clearchannel", description="Clears a channel without the pinned messages", guild_ids=[int(info.f1abeezID),  int(info.f2abeezID), int(info.testServerID)])
+async def clearChannel(interaction: Interaction):
+  if(utils.check_roles(interaction.user.roles, ["Admin", "Moderator"])):
+    await interaction.response.defer()
+    def check(m: nextcord.Message):
+      return m.pinned == False
+    await interaction.channel.purge(check=check)
+  else:
+    await interaction.send("You do not have permission to use this command!")
+
+
+
 for fn in os.listdir("./cogs"):
   if fn.endswith(".py"):
     logging.info("Loading %s", fn)
