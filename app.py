09c493806f4d01c8c2797d9947876ae6355b0879
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-06-26 17:13:30 +0200

    moved on_member_join

--- app.py
+++ app.py
@@ -388,24 +388,6 @@ async def ban(interaction: Interaction, user: Member = SlashOption(name="user", 
     await interaction.send(embed = embed)   
   else:
     await interaction.send("You do not have permission to use this command!")
-    
-@bot.event
-async def on_member_join(member):
-  serverID = member.guild.id
-  if(member.guild.id == int(info.f1abeezID)):
-    role = nextcord.utils.get(member.guild.roles, name="Academy Driver")
-    await member.add_roles(role)
-  elif(member.guild.id == int(info.f2abeezID)):
-    role = nextcord.utils.get(member.guild.roles, name="Member")
-    await member.add_roles(role)
-  channel = bot.get_channel(info.get_channelID(serverID, "welcomeChannel"))
-  if(type(channel) != type(None)):
-    if(member.guild.id == int(info.f1abeezID)):
-      await channel.send(f"**Welcome {member.mention}**\n\nPlease use this chat if you have any questions and someone will be on hand.\n\nAll the information you need is on <#865379267977412618>")
-    elif(member.guild.id == int(info.f2abeezID)):
-      await channel.send(f"Welcome to F2ABEEZ!{member.mention}\n\nYour dedicated F2 racing discord community. Please read <#937998062842957824> to get equated with our brand and information then, head over to <#937997355737833482> to get a seat in the next race that suits your pace! Please **put your gamertag into brackets behind your discord name**, thank you!")
-  else:
-    logging.error("welcomeChannel not found")
 
 @bot.event
 async def on_member_remove(member):
